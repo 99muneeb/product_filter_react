221d5670874c6c7a0d75bb2e02496bec73e33807
99muneeb <muneeb9166@gmail.com>
2023-04-02 14:05:57 +0500

    rating and ui update

--- src/Menu.js
+++ src/Menu.js
@@ -4,16 +4,18 @@ const Menu = ({ products }) => {
   return (
     <div className="section-center">
       {products.map((product) => {
-        const { id, title, image, description, price } = product;
+        const { id, title, image, description, price ,rating  } = product;
         return (
           <article key={id} className="menu-item">
             <img src={image} alt={title} className="photo" />
             <div className="item-info">
-              <header>
+              <header style={{display:'flex' ,flexDirection:'column' ,paddingBottom:'20px'}}>
                 <h4>{title}</h4>
                 <h4 className="price">RS {price}</h4>
+                <h5>Rating: {rating.rate} <span className="price">({rating.count} reviews)</span></h5>
               </header>
               <p className="item-text">{description}</p>
+              
             </div>
           </article>
         );
